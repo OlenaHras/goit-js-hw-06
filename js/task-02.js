b5b7a5b214fa906430ed87d47f51331b5d0beb9a
OlenaHras <alena.hras@gmail.com>
2022-07-21 20:48:38 +0300

    task-2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = document.querySelector('#ingredients');
+const elements = [];
+const listOfItems = ingredients.forEach(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+
+  elements.push(itemEl);
+});
+
+listEl.append(...elements);
+
