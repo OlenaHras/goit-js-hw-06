993322d7eca042e8ef377e426077b930414d21df
OlenaHras <alena.hras@gmail.com>
2022-07-22 14:03:31 +0300

    Update task-5

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const textInput = document.querySelector('#name-input');
+const textOutput = document.querySelector('#name-output');
+
+textInput.addEventListener('input', (event) => {
+  textOutput.textContent = event.currentTarget.value;
+  if (event.currentTarget.value.length ===0) {
+   textOutput.textContent = 'Anonymous';
+  }
+})
