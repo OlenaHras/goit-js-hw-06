17c4a790d6103243983fb101f87a33e94f9d5abd
OlenaHras <alena.hras@gmail.com>
2022-07-22 17:12:13 +0300

    task-06 done

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const textInput = document.querySelector('#validation-input');
+const dataLength = textInput.getAttribute('data-length');
+
+function onInputBlur(event) {
+  
+  if (event.target.value.length !== Number(dataLength)) {
+  return textInput.classList.add('invalid');
+  }
+  
+  textInput.classList.remove('invalid');
+  return textInput.classList.add('valid');
+}
+
+textInput.addEventListener('blur', onInputBlur);
