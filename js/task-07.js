143bc081b708ff50d932ad34c6259bacd847a866
OlenaHras <alena.hras@gmail.com>
2022-07-22 17:39:21 +0300

    task-7 done

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+// console.log(text.textContent);
+function onChangeInput() {
+  const inputValue = Number(input.value);
+  text.style.fontSize = `${inputValue}px`;
+}
+input.addEventListener('change', onChangeInput);
