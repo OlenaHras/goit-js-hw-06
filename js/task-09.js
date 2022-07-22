2ebb05e219b06925bb5edf76fa1967ed8c921d26
OlenaHras <alena.hras@gmail.com>
2022-07-22 20:35:40 +0300

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector('.change-color');
+const text = document.querySelector('.color');
+
+button.addEventListener('click', changeColor);
+
+function changeColor() {
+  text.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = `${text.textContent}`;
+}
+
+
