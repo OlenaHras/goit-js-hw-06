658fed0877734cea1dfb12f8c87ea21236c8fb3b
OlenaHras <alena.hras@gmail.com>
2022-07-22 18:57:35 +0300

    task-8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  
+  if (mail.length === 0 || password.length === 0) {
+  return  alert('All fields must be filled');
+  }
+
+  const formData = {
+    mail,
+    password,
+  }
+  console.log(formData)
+
+  event.currentTarget.reset();
+}
